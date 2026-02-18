3ef5b2b8862b6fe1ed368cf0562a00bac1018683
Prachi Ikhar <PrachiVilasrao.Ikhar@cognizant.com>
2026-02-18 10:44:36 +0530

    First Commit

---
+++ Personal/MyFirst.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Myfirst
+
+{
+    static void Main(string[] args)
+    {
+        int i= 56;
+        Console.WriteLine("Hello, World!"+i);
+        // wait        
+        double,bool double,string
+        Console.WriteLine("Press key to exit");
+       
+    }
+}
